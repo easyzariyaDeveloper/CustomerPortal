903c70dd9400713051d55c79e19abd53adc94140
Gopal Krishna <gopalkrishna@Naveens-MacBook-Pro.local>
2020-12-02 10:20:48 +0530

    changing endpoint

--- src/constant.js
+++ src/constant.js
@@ -2,8 +2,8 @@ import devServer from "../webpack-devserver.config";
 
 //http://localhost:8080
 // export const API_ENDPOINT = "https://easyzariyademo.azurewebsites.net/";
-export const API_ENDPOINT = "http://api.ezautocare.in";
-export const SITE_ENDPOINT = "http://api.ezautocare.in";
+export const API_ENDPOINT = "http://ezautocare.in";
+export const SITE_ENDPOINT = "http://ezautocare.in";
 export const BASE_API_ENDPOINT = process.env.NODE_ENV !== "production" ? 
     `http://localhost:${devServer?.port}/api` : 
     `${SITE_ENDPOINT}/api`
